5506d5a5e75a077a303a7e2bd93b536e279c83cc
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2017-04-28 04:11:27 +0200

    update spiders

--- portia_projects/hoaxlyPortia/pipelines.py
+++ portia_projects/hoaxlyPortia/pipelines.py
@@ -9,7 +9,7 @@ class TypePipeline(object):
     def process_item(self, item, spider):
         """implements https://doc.scrapy.org/en/latest/topics/item-pipeline.html#process_item"""
         if item['_type']:
-            item['type'] = item.pop('_type')
+            item['schema'] = item.pop('_type')
             return item
         else:
             raise DropItem("couldnt rename type column for %s" % item)
