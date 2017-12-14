bc3c6b946b20a376faad9374d332cb1d16b4d2a0
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2017-12-14 03:15:44 +0100

    dont mount project folder directly but deploy from seperate cli container

--- portia_projects/hoaxlyPortia/spiders/settings.py
+++ portia_projects/hoaxlyPortia/spiders/settings.py
@@ -1,10 +1,14 @@
 # Automatically created by: slyd
 import os
+import hoaxlyHelpers
+import scrapyelasticsearch
+import slybot
 
 #SPIDER_MANAGER_CLASS = 'slybot.spidermanager.ZipfileSlybotSpiderManager'
-#SPIDER_LOADER_CLASS = 'slybot.spidermanager.ZipfileSlybotSpiderManager'
-SPIDER_LOADER_CLASS = 'slybot.spidermanager.SlybotSpiderManager'
-EXTENSIONS = {'slybot.closespider.SlybotCloseSpider': 10}
+SPIDER_LOADER_CLASS = 'slybot.spidermanager.ZipfileSlybotSpiderManager'
+#SPIDER_LOADER_CLASS = 'slybot.spidermanager.SlybotSpiderManager'
+EXTENSIONS = {'slybot.closespider.SlybotCloseSpider': 1}
+
 DOWNLOADER_MIDDLEWARES = {
     'slybot.pageactions.PageActionsMiddleware': 700,
     'slybot.splash.SlybotJsMiddleware': 725
@@ -13,26 +17,20 @@ PLUGINS = [
     'slybot.plugins.scrapely_annotations.Annotations',
     'slybot.plugins.selectors.Selectors'
 ]
-SLYDUPEFILTER_ENABLED = True
-DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+SLYDUPEFILTER_ENABLED = False
+#DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 
 SPIDER_MIDDLEWARES = {
-    # the one mounted in docker
-    'slybot.mymiddleware.MicrodataExtruction': 643,
-    # the one deployed as egg
-    #'spiders.MicrodataExtruction': 643,
-
+    'hoaxlyHelpers.mymiddleware.MicrodataExtruction': 643,
     'slybot.spiderlets.SpiderletsMiddleware': 999
+
 }
 
 ITEM_PIPELINES = {
     'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline': 600,
 
-    # the one deployed as egg
-    #'spiders.TypePipeline': 500,
-    # the one mounted in docker
-    'slybot.mypipelines.TypePipeline': 2,
-    'slybot.dupefilter.DupeFilterPipeline': 1
+    'hoaxlyHelpers.mypipelines.TypePipeline': 3,
+    'slybot.dupefilter.DupeFilterPipeline': 700
 }
 
 ELASTICSEARCH_SERVERS = ['http://elastic:changeme@elastic:9200']
@@ -42,8 +40,9 @@ ELASTICSEARCH_UNIQ_KEY = 'url'
 ELASTICSEARCH_INDEX_DATE_FORMAT = '%Y-%m'
 
 # ugly but hardcoding works
-PROJECT_DIR = '/app/data/projects/hoaxlyPortia'
-#PROJECT_ZIPFILE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+#PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+#PROJECT_DIR = '/app/slyd/eggs/HoaxlyPortia/'
+PROJECT_ZIPFILE = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 try:
     from local_slybot_settings import *
